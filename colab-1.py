91f113e6a891572d2d7325b3270e1328c08410f5
prof-carlos-colab <profcarloscolab@gmail.com>
2024-10-31 11:56:40 +0000

    Add colab-1.py

---
+++ colab-1.py
@@ -0,0 +1,24 @@
+import os
+
+os.system("cls || clear")
+
+print("Colaborador - 1")
+
+# COMANDOS DO GIT
+
+# Atualiza o repositório atual com dados do repositório no GitHub.
+# git pull
+
+# Adiciona novos dados para o commit.
+# git add .
+
+# Versiona com os novos dados.
+# git commit -m "Título para o commit"
+
+# Atualiza trazendo commits do GitHub.
+# git pull
+
+# Envia os commits para o GitHub
+# git push
+
+
